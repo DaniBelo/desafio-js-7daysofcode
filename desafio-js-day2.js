75200bc3cab6320e9a901f66f14d150c876f095f
DaniBelo <133611933+DaniBelo@users.noreply.github.com>
2023-06-25 15:56:24 -0300

    Adicionado o arquivo do dia 2

---
+++ desafio-js-day2.js
@@ -0,0 +1,6 @@
+const nomeUsuario = prompt("Digite o seu nome?");
+const idadeUsuario = prompt("Digite a sua idade?");
+const linguagemPrgramacao = prompt("Digite a linguagem de programação que você está estudando?");
+const mensagem = `Olá ${nomeUsuario}, você tem ${idadeUsuario} anos e está aprendendo ${linguagemPrgramacao}!`;
+
+alert(mensagem);
