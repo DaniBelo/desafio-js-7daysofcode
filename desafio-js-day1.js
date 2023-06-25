8e1c952c63d4e7e6366e976a30dfc0218e3002c7
DaniBelo <133611933+DaniBelo@users.noreply.github.com>
2023-06-25 11:38:27 -0300

    Adicionado novo arquivo do dia 1

---
+++ desafio-js-day1.js
@@ -0,0 +1,24 @@
+let numeroTres = 3
+let stringTres = '3'
+let numeroVinte = 20
+let stringVinte = '20'
+let numeroOito = 8
+let stringOito = '8'
+
+if (numeroTres == stringTres) {
+  console.log('As variáveis numeroTres e stringTres tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroTres e stringTres não tem o mesmo valor')
+}
+
+if (numeroVinte === stringVinte) {
+  console.log('As variáveis numeroVinte e stringVinte tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroVinte e stringVinte não tem o mesmo tipo')
+}
+
+if (numeroOito == stringOito) {
+  console.log('As variáveis numeroOito e stringOito tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroOito e stringOito não tem o mesmo valor')
+}
